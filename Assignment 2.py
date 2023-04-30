c10f2c52709b23f5680a0345b353b71a902d4223
Reyhaneghoreyshi <132213256+Reyhaneghoreyshi@users.noreply.github.com>
2023-04-30 21:48:38 +0330

    Add files via upload
    Assignment 2

---
+++ Assignment 2.py
@@ -0,0 +1,11 @@
+name=input("enter the name")
+lastname=input("enter the the last name")
+average=(float(input("enter the course1"))+float(input("enter the course2"))+float(input("enter the course3")))/3
+print(average)
+if  average >=17 :
+     print("great")
+if   17 >average>= 12 :
+     print("normal")
+if average < 12 :
+     print("fail")
+print(name,lastname,average)
